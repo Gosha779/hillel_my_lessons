8e76c3a08cfcde099c644dd43f7efd51aa661470
Gosha <treestar.ua.fm>
2024-11-30 17:25:04 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+let someValue1 = "myname";
+var someValue2 = "anonimous";
+
+let prodStoreHouseCap = 100;
+let n, x, a, b, c;
+const DAYS_IN_YEAR = 365;
+const massiv = [43,34,6,78,98,23];
+console.log("Masyv constants",massiv);
+let notNumber = 4/0;
+console.log(notNumber);
+let x1 = 55;
+let c1 = 77;
+let bigNum = 8888888888888888888888888888888888888n;
+console.log(bigNum);
+let text = "Cytation:\n \"This\"";
+console.log(text);
+let user = {
+name:"Gosha",
+age:12
+
+}
+console.log(user.name);
+
+let teza = true;
+console.log(1+teza);
+console.log(Boolean(""));
+
+let num1 = 5;
+
+console.log(num1%3);
+
+if(num1 === 6) {
+
+    alert(num1);
+}else {
+
+    console.log("NE SPRAVILOS")
+}
+let num_ = 10;
+console.log(num_ < 100 ? "Menshe 100" : "Dilshe abo ravno")
+let com = `name: ${"Ytllo"}! he is ${c1} years old`;
+for(let i=1;i<=10;i++) {
+    console.error(com); 
+                 
+}
+
+console.log(typeof(n));
+Math.trunc
